51b1c1fdba33c1ad9dac995925bd969c9843e19a
ViniciusCamargo10 <viniciusc.2005@gmail.com>
2025-08-18 15:22:36 -0300

    up

--- Frontend/Python/PanilhaP/Json.py
+++ Frontend/Python/PanilhaP/Json.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Caminho do arquivo Excel
-excel_file = "C:\Users\s1337626\Downloads\DietaAguda_Calculadora_V4.xlsx"
+excel_file = "C:\\Users\\s1337626\\Downloads\\DietaAguda_Calculadora_V4.xlsx"
 
 # Ler a planilha
 df = pd.read_excel(excel_file)
 
 # Converter para JSON
-json_file = "dieta_aguda_v2.json"
+json_file = "acute_crop.json"
 df.to_json(json_file, orient="records", force_ascii=False)
 
 print(f"Arquivo JSON salvo em: {json_file}")
--- Frontend/javascript/Botoes.js/acute_crop.js
+++ Frontend/javascript/Botoes.js/acute_crop.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
   const calculatorBtn = document.getElementById("btn-calculator");
   if (!calculatorBtn) {
-    console.warn("Botão Home (#btn-calculator) não encontrado na calculadora.");
+    console.warn("Botão Calculator (#btn-calculator) não encontrado na calculadora.");
     return;
   }
 
@@ -100,3 +100,46 @@ function abrirModal() {
             modal.style.display = 'none';
         }
     };
+
+
+fetch('../../javascript/Botoes.js/acute_crop.json')
+  .then(response => response.json())
+  .then(data => {
+    renderTabela(data);
+  });
+
+function renderTabela(dados) {
+  const tabela = document.getElementById('tabela-dados');
+  tabela.innerHTML = '';
+  tabela.innerHTML += `
+    <tr>
+      <th>Cultivo</th>
+      <th>Ano POF</th>
+      <th>Região</th>
+      <th>Caso Fórmula</th>
+      <th>LMR (mg/kg)</th>
+      <th>HR/MCR (mg/kg)</th>
+      <th>MREC/STMR (mg/kg)</th>
+      <th>IMEA (mg/kg p.c/dia)</th>
+      <th>%DRFA ANVISA</th>
+      <th>%DRFA SYNGENTA</th>
+    </tr>
+  `;
+  dados.forEach(item => {
+    tabela.innerHTML += `
+      <tr>
+        <td>${item['Cultivo/ Matriz Animal'] || ''}</td>
+        <td>${item['ANO POF'] || ''}</td>
+        <td>${item['Região'] || ''}</td>
+        <td>${item['Caso Fórmula'] || ''}</td>
+        <td>${item['LMR (mg/kg)'] || ''}</td>
+        <td>${item['HR/MCR (mg/kg)'] || ''}</td>
+        <td>${item['MREC/STMR (mg/kg)'] || ''}</td>
+        <td>${item['IMEA (mg/kg p.c./dia)'] || ''}</td>
+        <td>${item['%DRFA ANVISA'] || ''}</td>
+        <td>${item['%DRFA SYNGENTA'] || ''}</td>
+      </tr>
+    `;
+  });
+}
+
