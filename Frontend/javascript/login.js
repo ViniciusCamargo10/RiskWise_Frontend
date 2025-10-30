3730bbca0691566962d8d44859e8f6eb2a3bf7e1
ViniciusCamargo10 <viniciusc.2005@gmail.com>
2025-10-30 10:25:23 -0300

    up

--- Frontend/javascript/login.js
+++ Frontend/javascript/login.js
@@ -10,13 +10,13 @@ const USERS = {
 };
 
 document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault(); // ✅ Impede envio padrão
+    e.preventDefault();
 
     const username = document.querySelector("[name=username]").value.trim();
     const password = document.querySelector("[name=password]").value.trim();
 
     if (USERS[username] && USERS[username] === password) {
-        localStorage.setItem("loggedIn", "true");
+        sessionStorage.setItem("loggedIn", "true");
         window.location.href = "index.html";
     } else {
         document.getElementById("errorMsg").style.display = "block";
